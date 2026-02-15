b71200eab7aa9a40c8721f2b5e9c4f0dcd949590
vishwas <kamathvishwasm@gmail.com>
2026-02-15 22:56:41 +0530

    Singleton pattern

---
+++ singleton/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package singleton;
+
+public class DatabaseConnection {
+    // 1. VOLATILE is critical here.
+    // It prevents "Instruction Reordering" so threads don't see a half-baked object.
+    private static volatile DatabaseConnection instance;
+
+    private DatabaseConnection() {
+        System.out.println("Connecting to Database...");
+    }
+
+    public static DatabaseConnection getInstance() {
+        // Check 1: Performance
+        // If it's already created, we skip the slow synchronized block entirely.
+        if (instance == null) {
+
+            // The Lock
+            synchronized (DatabaseConnection.class) {
+
+                // Check 2: Safety
+                // We check again in case Thread B created it while Thread A was waiting outside the lock.
+                if (instance == null) {
+                    instance = new DatabaseConnection();
+                }
+            }
+        }
+        return instance;
+    }
+}
